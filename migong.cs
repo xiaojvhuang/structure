691f7c503be43805dbfb9da3f07e972879febc94
junkaih@qq.com <junkaih@qq.com>
2020-07-19 16:24:37 +0800

    迷宫

---
+++ migong.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace 数据结构
+{
+    //迷宫
+    public class MiGong
+    {
+        public void main(string[] args)
+        {
+            //地图
+            int[,] map = new int[8, 8];
+            for (int i = 0; i < 8; i++)
+            { }
+        }
+    }
+}
