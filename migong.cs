8bc0205dac33130002db0740cdf110714fcd01a7
junkaih@qq.com <junkaih@qq.com>
2020-07-19 16:36:24 +0800

    迷宫

--- migong.cs
+++ migong.cs
@@ -10,7 +10,13 @@ namespace 数据结构
             //地图
             int[,] map = new int[8, 8];
             for (int i = 0; i < 8; i++)
-            { }
+            {
+                map[0, i] = 1;
+                map[7, i] = 1;
+            }
+
+            //打印地图
+            foreach()
         }
     }
 }
